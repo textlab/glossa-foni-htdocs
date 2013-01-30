fff488b03ef32e26cf093f0178be86a737752da3
Michał Kosek <michalkk@student.iln.uio.no>
2013-01-30 17:31:06 +0100

    Files related to the SKRIV corpus

---
+++ js/skriv.conf.js
@@ -0,0 +1,12 @@
+var conf = new Array;
+
+var languageOpts = new Array;
+languageOpts = [['SKRIV', 'Norsk']];
+conf['charset'] = 'UTF-8';
+conf['type'] = 'monolingual';
+conf['title'] = 'Search the student writing corpus';
+conf['corpus_name'] = 'SKRIV-korpuset';
+var language='en';
+
+var cgiRoot = 'http://tekstlab.uio.no/cgi-bin/glossa/';
+var htmlRoot = 'http://tekstlab.uio.no/glossa/';
