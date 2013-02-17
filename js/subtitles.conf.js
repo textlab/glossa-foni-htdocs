d2ed54817ebc038f601d4723bc0805c4e2bf673e
andrely <tekstlab-post@iln.uio.no>
2013-02-17 15:21:14 +0100

    Files needed for the subtitles corpus.

---
+++ js/subtitles.conf.js
@@ -0,0 +1,17 @@
+var languageOpts = new Array;
+
+languageOpts = [
+  ['SUBTITLES_NO', 'Norwegian','no'],
+  ['SUBTITLES_EN', 'English', 'en'], 
+];
+
+
+var conf = new Array;
+
+conf['type'] = 'multilingual';
+conf['title'] = 'Search the Subtitle Corpus';
+conf['corpus_name'] = 'The Subtitle Corpus';
+conf['charset'] = 'UTF-8';
+var language='en';
+
+var cgiRoot = 'http://tekstlab.uio.no/cgi-bin/glossa/';
