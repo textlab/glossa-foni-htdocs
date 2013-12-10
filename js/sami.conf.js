e5fd9160ee67971d56281f2a914d72748ee897dd
Tekstlab <tekstlab-post@iln.uio.no>
2013-12-10 14:57:56 +0100

    Add sami corpus

---
+++ js/sami.conf.js
@@ -0,0 +1,9 @@
+var conf = new Array;
+
+var languageOpts = new Array;
+languageOpts = [['SAMI', 'Sami']];
+
+conf['type'] = 'monolingual';
+conf['title'] = 'Search Sami corpus';
+conf['corpus_name'] = 'Corpus of Modern Sami';
+var language='en';
