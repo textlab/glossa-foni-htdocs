9a492c9c4f9fc745f20747e31d438a2cf344e9ea
Michał Kosek <michalkkpleaseremovethis@student.iln.uio.no.fake>
2014-11-04 17:35:37 +0100

    Make displayStruct work not only on the first result page

--- js/reslist.js
+++ js/reslist.js
@@ -1,4 +1,4 @@
-function showList(No,String,Label1,Hits,Label2,Corpus,Max,atttype,player, cgiRoot, concordance_for_speech) {
+function showList(No,String,Label1,Hits,Label2,Corpus,Max,atttype,player, cgiRoot, concordance_for_speech, struct_display) {
     if( arguments.length > 7){ atttype = "&atttype=" + atttype; }
     if( arguments.length > 8){ player = "&player=" + player; }
     concordance_for_speech = "&concordance_for_speech=" +concordance_for_speech;
@@ -15,7 +15,7 @@ function showList(No,String,Label1,Hits,Label2,Corpus,Max,atttype,player, cgiRoo
 	  for (var i = 1;i<=No;i++) {
 		    if (i==1) { HTML=HTML+"<font size=\"+1\"><b>" }
 		    HTML = HTML + "<a href=\"" + cgiRoot + "/show_page_dev.cgi?n=" + i
-            +"&query_id="+String+"&corpus="+Corpus+atttype+player+concordance_for_speech+"\"> "+i+"</a> ";
+            +"&query_id="+String+"&corpus="+Corpus+atttype+player+concordance_for_speech+'&structDisplay='+struct_display+"\"> "+i+"</a> ";
 
         if ( i==1 ) {
             HTML=HTML+"</b></font>";
